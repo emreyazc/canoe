e8936a4bafa70e9f4fa4a1c71d94e7e599dcb282
Thomas Arellano <Thomas.G.Arellano@gmail.com>
2016-01-29 22:57:42 -0500

    Initial UI

--- canoe/Form1.cs
+++ canoe/Form1.cs
@@ -12,9 +12,33 @@ namespace canoe
 {
     public partial class Form1 : Form
     {
+        private static int size = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbSize_TextChanged(object sender, EventArgs e)
+        {
+            string t = tbSize.Text;
+            size = Int32.Parse(t);
+
+
+            Label lbl = new Label();
+            lbl.Text = size.ToString();
+            lbl.Location = new Point(100, 100);
+            this.Controls.Add(lbl);
+
+        }
+
+        private void tbSize_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Use this function to track changes to textbox on key presses.
         }
     }
 }
